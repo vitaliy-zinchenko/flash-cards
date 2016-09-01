4524ae90e373c80b9091e47a1528c347456ee7a9
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-09-01 13:02:22 +0300

    include ngResource

--- src/main/resources/routes.js
+++ src/main/resources/routes.js
@@ -1,5 +1,3 @@
-//import cardsComponent from './static/app/view-sets';
-
 export default function ( $stateProvider, $urlRouterProvider, $locationProvider ) {
   //'ngInject'; // TODO: use ng-annotate
 
@@ -9,18 +7,5 @@ export default function ( $stateProvider, $urlRouterProvider, $locationProvider 
   } );
 
   $urlRouterProvider.otherwise( '/main' );
-
-  $stateProvider
-/*    .state( 'sets', {
-      url: '/main',
-      //abstract: true, // TODO: read about this param
-      template: cardsComponent
-    } )*/
-    .state( 'card-set', {
-      url: '/card-set?page=0&size=10',
-      //abstract: true, // TODO: read about this param
-      template: '<h1>SET</h1>'
-      //controller: 'cardsController'
-    } )
 
 }
--- src/main/resources/static/app/view-sets/sets.controller.js
+++ src/main/resources/static/app/view-sets/sets.controller.js
@@ -1,9 +1,15 @@
-export default class setsController {
-  constructor() {
+//import setsService from './../services/sets.service';
+
+export default class setsController  {
+  constructor($resource) {
     this.name = 'SETS';
+
+    let res  = $resource('/card-set?page=0&size=10');
+    this.data = res.query();
   }
 
   changeName() {
     this.name = 'angular-tips';
   }
 }
+
