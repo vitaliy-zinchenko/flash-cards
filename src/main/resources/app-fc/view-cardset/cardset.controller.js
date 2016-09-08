71ea7e6c51cb5f806822f1362fe6eaf35807a52c
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-09-08 16:46:52 +0300

    Create cards (but uncorrect url)

--- src/main/resources/app-fc/view-cardset/cardset.controller.js
+++ src/main/resources/app-fc/view-cardset/cardset.controller.js
@@ -3,6 +3,7 @@ export default class cardsetController {
     this.name = 'cardsetController';
 
     this.currentSet = null;
+    this.newCards = [];
 
     var page = 0;
     var size = 9999999;
@@ -28,16 +29,48 @@ export default class cardsetController {
       console.log('delete card');
     };
 
-    this.addCard = () => {
+    // save terms to temporary array (this.newCards)
+    this.writeTerm = (newTerm) => {
+      console.log(newTerm);
+      var obj = this.copyObj(newTerm);
+      this.newCards.push(obj);
+      console.log(this.newCards);
+      this.newterm = null;
+    };
+
+    this.newCard = () => {
       console.log('new card');
     };
 
-    this.saveCard = (cards) => {
-      cardsService.createSet( cards ).$promise.then(data => {
-        //console.log('Create new set:');
+    this.saveCards = (cards) => {
+      console.log(cards);
+// send temp array to server
+      cardsService.addCards( id, cards ).$promise.then(data => {
         console.log(data);
       });
+
+      // and clean temp array
+      this.newCards = [];
+
+      // than add responce objects to existing cards
+      var respData = [ {
+        "id" : 10,
+        "word" : "word1",
+        "translation" : "translation1"
+      } ];
+
+      respData.forEach((item, i) => {
+        this.cards.push(item);
+      })
+    };
+
+// TODO: helper function. Need refactor
+    this.copyObj = (obj) => {
+      var copy = {};
+      for (var key in obj) {
+        copy[key] = obj[key];
+      }
+      return copy;
     }
   }
-
 }
