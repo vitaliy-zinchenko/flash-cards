7b8ae996d57d6a0da2baa64f4a706d2d6accab66
Vitaliy_Zinchenko <zinjvi@gmail.com>
2016-09-10 14:03:39 +0300

    changed build qa script

--- scripts/build-qa.bat
+++ scripts/build-qa.bat
@@ -1,11 +1,24 @@
 @echo off
 
+echo 'Setup variables...'
+
 SET DOCKER_TLS_VERIFY=1
 SET DOCKER_HOST=tcp://104.236.116.198:2376
 SET DOCKER_CERT_PATH=C:\Users\Vitaliy_Zinchenko\.docker\machine\machines\docker1
 SET DOCKER_MACHINE_NAME=docker1
 
-SET base_dir=%FLASH_CARDS_HOME%
+echo 'Building...'
 
-mvn clean package -P build-docker-image
+call mvn clean package -P build-docker-image
 
+if NOT %errorlevel%==0 (
+	echo *************** ERROR ***************
+	exit /b
+)
+
+pushd docker
+
+echo 'Deploying...'
+call docker-compose up -d
+
+popd
