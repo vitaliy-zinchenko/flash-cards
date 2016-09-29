5854c0d90c128e579b80814dae8d6cb6def43bdc
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-09-29 13:01:49 +0300

    refactoring cardset component

--- src/main/ui/app-fc/view-cardset/cardset.controller.js
+++ src/main/ui/app-fc/view-cardset/cardset.controller.js
@@ -1,69 +1,63 @@
 export default class cardsetController {
   /* @ngInject */
-  constructor(cardsService, $state) {
+  constructor(cardsService, $state, $q) {
     var vm = this;
-
-    vm.name = 'cardsetController';
-
-    vm.currentSet = null;
-    vm.newCards = [];
-    vm.cards = []; //TODO hotfix without this angular.js:13920 TypeError: Cannot read property 'push' of undefined. During saving new card in new card set
-
-    var page = 0;
-    var size = 9999999;
-    var id = $state.params.id;
-
-//create set
-    vm.saveSet = (set) => {
-      setService.createSet(set).$promise.then(data => {
-       console.log('Create new set:');
-       console.log(data);
-       vm.currentSet = data;
-       id = data.id; //TODO this is hotfix. without this one new cards for new cards set are sent to /api/card-set/new/cards/batch
-       });
-    };
-
-//get existing cards
-    if( id && id != 'new') { //TODO is it possible to avoid chis check?
-      vm.currentSet = 'set'; //TODO: get particular set by id
-      vm.cards = cardsService.getAll(id, page, size);
-    }
-
-//cards
-    vm.deleteCard = () => {
-      console.log('delete card');
-    };
-
-    vm.newCard = () => {
-      var card = {
-        word: "",
-        translation: ""
-      };
-
-      vm.cards.push(card);
-    };
-
-
-    vm.createCard = (card) => {
-      var sendData = vm.tempPrepare(card); // TODO: remove this when update backend method
-      vm.saveCards(sendData).then(
-        response => card = response
-      );
-    };
-
-    // temp function for convert obj to array
-    vm.tempPrepare = (obj) => { // TODO: remove this when update backend method
-      return [obj];
-    };
-
-    // save card on server
-    vm.saveCards = (cards) => {
-      return new Promise(function(resolve, reject) {
-        cardsService.addCards( id, cards ).$promise.then(data => {
-          resolve(data[0]);
-        });
-      });
-    };
-
+    vm._cardsService = cardsService;
+    this.$state = $state;
+    this.initialize();
   }
+
+
+  //cards
+  newCard() {
+    var card = {
+      word: "",
+      translation: ""
+    };
+    this.cards.push(card);
+  };
+
+  createCard(card) {
+    var sendData = this.tempPrepare(card); // TODO: remove this when update backend method
+    this.saveCards(sendData).then(
+      response => card = response
+    );
+  };
+
+  deleteCard() {
+    console.log('delete card');
+  };
+
+
+  // temp function for convert obj to array
+  tempPrepare(obj) { // TODO: remove this when update backend method
+    return [obj];
+  };
+
+  // save card on server
+  saveCards(cards) {
+    return new Promise(function(resolve, reject) {
+      debugger;
+      cardsService.addCards( this.id, cards ).$promise.then(data => {
+        resolve(data[0]);
+      });
+    });
+  };
+
+
+  initialize() {
+    this.name = 'cardsetController';
+    this.currentSet = null;
+    this.cards = []; //TODO hotfix without this angular.js:13920 TypeError: Cannot read property 'push' of undefined. During saving new card in new card set
+    this.page = 0;
+    this.size = 9999999;
+    this.id = this.$state.params.id;
+
+    //get existing cards
+    if( this.id && this.id != 'new') { //TODO is it possible to avoid chis check?
+      this.currentSet = 'set'; //TODO: get particular set by id
+      this.cards = this._cardsService.getAll(this.id, this.page, this.size);
+    }
+  }
+
 }
--- src/main/ui/app-fc/view-cardset/set-title/set-title.controller.js
+++ src/main/ui/app-fc/view-cardset/set-title/set-title.controller.js
@@ -1,21 +1,32 @@
 export default class setTitleController {
   /* @ngInject */
-  constructor($state, setService) {
-    var vm = this;
-    vm.name = 'setTitleController';
+  constructor($state, setService, $q) {
+    this.$state = $state;
+    this.$q = $q;
+    this._setService = setService;
 
-    var id = $state.params.id;
-
-    //create new set
-    vm.saveSet = (set) => {
-      setService.createSet(set).$promise.then(data => {
-        console.log('Create new set:');
-        console.log(data);
-        vm.currentSet = data;
-        debugger;
-        id = data.id; //TODO this is hotfix. without this one new cards for new cards set are sent to /api/card-set/new/cards/batch
-      });
+    this.initialize();
     };
 
-    };
+
+
+  /*----------------*/
+  initialize() {
+    this.name = 'setTitleController';
+    this.id = this.$state.params.id;
+  }
+
+  /*----------------*/
+
+//create new set
+  saveSet(set) {
+    this._setService.createSet(set).$promise.then(data => {
+      console.log('Create new set:');
+      console.log(data);
+      this.currentSet = data;
+      this.id = data.id; //TODO this is hotfix. without this one new cards for new cards set are sent to /api/card-set/new/cards/batch
+    });
+  };
+
+
 }
