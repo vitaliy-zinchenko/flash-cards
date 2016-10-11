3e0bf417f73c06c4540f6d12ab3dcf62dddabc9e
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-10-11 18:52:41 +0300

    add Satellizer modul for authentication

--- src/main/ui/app-fc/view-signin/signin.module.js
+++ src/main/ui/app-fc/view-signin/signin.module.js
@@ -1,10 +1,21 @@
 import angular from 'angular';
 
+import satellizer from 'satellizer';
+
 import viewComponent    from './signin.component.js';
 
-export default angular.module('viewSighin', [])
-  .config(/* @ngInject */$stateProvider => {
+export default angular.module('viewSighin', [satellizer])
+  .config(/* @ngInject */($stateProvider, $authProvider) => {
     $stateProvider
-      .state('signin', viewComponent)
+      .state('signin', viewComponent);
+
+    $authProvider.google({
+      clientId: '563875364656-v4t1v5t4agj9p57m35q9t6f9rgrn0r02'
+    });
+
   })
+
   .name;
+
+
+
