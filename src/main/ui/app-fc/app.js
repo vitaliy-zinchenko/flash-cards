b8b3ebf3b69c4f210bba53dc7e566a3719b75f74
Vitaliy_Zinchenko <zinjvi@gmail.com>
2016-10-18 12:19:58 +0300

    login server side impl

--- src/main/ui/app-fc/app.js
+++ src/main/ui/app-fc/app.js
@@ -16,7 +16,7 @@ import footer from './components/footer';
 import header from './components/header';
 
 
-export default angular
+var fcApp = angular
   .module( 'fcApp', [
     uiRouter,
     ngResource,
@@ -29,9 +29,31 @@ export default angular
   .config(routes)
   .controller('mainController', mainController);
 
+fetchAppConfig().then(bootstrapApplication)
+
 function mainController() { //test code
   console.log('main app');
 }
 
+function fetchAppConfig() {
+    var initInjector = angular.injector(["ng"]);
+    var $http = initInjector.get("$http");
+
+    return $http.get("/api/config/app").then(function(response) {
+        fcApp.constant("config", response.data);
+        console.log(response.data)
+    }, function(errorResponse) {
+        //TODO handle
+        console.log("Error during loading configuration in bootstrap")
+    });
+}
+
+function bootstrapApplication() {
+    angular.element(document).ready(function() {
+        console.log("Bootstrapping App...")
+        angular.bootstrap(document, ["fcApp"]);
+        console.log("Bootstrapped App")
+    });
+}
 
 
