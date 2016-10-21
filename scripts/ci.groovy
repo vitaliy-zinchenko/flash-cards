ee7c0be1acc106cecf4eecc04c61ae1a7d629a6b
Vitaliy_Zinchenko <zinjvi@gmail.com>
2016-10-21 17:47:28 +0300

    ci config

--- scripts/ci.groovy
+++ scripts/ci.groovy
@@ -1,21 +1,18 @@
-node {
-    stage "Build maven & Docker image"
-    git url: 'git@github.com:vitaliy-zinchenko/flash-cards.git', branch: '${branch}'
-    sh "mvn clean package -P build-docker-image,qa"
+stage "Build maven & Docker image"
+sh "mvn clean package -P build-docker-image,qa"
 
-    stage "Saving Docker image to tar"
-    sh "docker save flashcards > flashcards.tar"
+stage "Saving Docker image to tar"
+sh "docker save flashcards > flashcards.tar"
 
-    stage "Copy tar to QA"
-    sh "scp flashcards.tar root@104.236.116.198:/tmp"
-    sh "scp docker/docker-compose.yml root@104.236.116.198:/tmp"
+stage "Copy tar to QA"
+sh "scp flashcards.tar root@104.236.116.198:/tmp"
+sh "scp docker/docker-compose.yml root@104.236.116.198:/tmp"
 
-    stage "Loading Docker image"
-    sh "ssh root@104.236.116.198 'docker load < /tmp/flashcards.tar'"
+stage "Loading Docker image"
+sh "ssh root@104.236.116.198 'docker load < /tmp/flashcards.tar'"
 
-    stage "Starting QA"
-    sh "ssh root@104.236.116.198 'cd /tmp && export FC_PORT=80 && export FC_PROFILE=qa && docker-compose -p flashcards_qa up -d --force-recreate'"
+stage "Starting QA"
+sh "ssh root@104.236.116.198 'cd /tmp && export FC_PORT=80 && export FC_PROFILE=qa && docker-compose -p flashcards_qa up -d --force-recreate'"
 
-    stage "Starting DEV"
-    sh "ssh root@104.236.116.198 'cd /tmp && export FC_PORT=9000 && export FC_PROFILE=dev && docker-compose -p flashcards_dev up -d --force-recreate'"
-}
+stage "Starting DEV"
+sh "ssh root@104.236.116.198 'cd /tmp && export FC_PORT=9000 && export FC_PROFILE=dev && docker-compose -p flashcards_dev up -d --force-recreate'"
