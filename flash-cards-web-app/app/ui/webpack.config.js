662f8ce5ce74e3be2f071bd84531104b2f5e8dd6
zinchenko-vitaliy <zinjvi@gmail.com>
2017-04-12 21:04:52 +0300

    refactoring

--- flash-cards-web-app/app/ui/webpack.config.js
+++ flash-cards-web-app/app/ui/webpack.config.js
@@ -1,16 +1,112 @@
+'use strict';
+
+// Parsing command line arguments
+var argv = require('minimist')(process.argv.slice(1));
+console.log("Command: " + JSON.stringify(argv));
+
+// Defaults variables
+const NODE_ENV = argv.mode || 'development';
+const DEV_PORT = argv.dev_port || 9999;
+const PROXY_TARGET = argv.proxy_target || 'http://flashcards-dev.xyz:9000';
+
+console.log("Running Webpack with NODE_ENV=" + NODE_ENV);
+
 var webpack = require('webpack');
+var AppCacheWebpackPlugin = require('appcache-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var path  = require('path');
+
+var srcPath     = path.join(__dirname, 'app-fc');
+var dstHomePath = path.join(__dirname, '/../../public');
+var dstPath     = path.join(dstHomePath, 'ui');
 
 module.exports = {
-    context: __dirname + '/app',
+    context: srcPath,
     entry: {
         app: './app.js',
         vendor: ['angular']  
     },
     output: {
-        path: __dirname + '/js',
+        path: dstPath,
         filename: 'app.bundle.js'
     },
+    watch: NODE_ENV == 'development',
+    devtool: NODE_ENV == 'development' ? "source-map" : null,
+    module: {
+      preLoaders: [
+        {
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ],
+      loaders: [
+//        {
+//          test: /\.js$/,
+//          loader: 'babel',
+//          query: {
+//            presets: ['es2015']
+//          },
+//          exclude: /node_modules/
+//        },
+
+        {
+          test: /\.css$/,
+          loader: "style-loader!css-loader"
+        },
+        { test: /\.appcache$/, loader: "file" },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+        { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+        {
+          test: /\.html$/,
+          loader: "html-loader"
+        }
+      ]
+    },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
-    ]
+        new CleanWebpackPlugin(['ui'], {
+          root: dstHomePath,
+          verbose: true,
+          dry: false
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+        new HtmlWebpackPlugin({
+          inject  : true,
+//          hash    : true,
+          template: 'index.html'
+        }),
+        new AppCacheWebpackPlugin({
+          network: ["*"],
+          output: 'manifest.appcache'
+        })
+    ],
+    devServer: {
+      host: 'localhost',
+      port : DEV_PORT,
+      contentBase: __dirname + "/app-fc",
+      proxy: [{
+          path: '/api',
+          target: PROXY_TARGET,
+          secure: false,
+          changeOrigin: true
+        }]
+    }
 };
+
+
+if (NODE_ENV == 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        // don't show unreachable variables etc
+        warnings:     false,
+        drop_console: true,
+        unused:       true,
+        dead_code:    true
+      }
+    })
+  );
+}
